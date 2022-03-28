6f7355a0eade37f5a98e08e125fd5ee8330596d6
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-03-28 10:40:00 +0900

    Initial Commit

---
+++ Shared/SocialProofSectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialProofSectionApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/03/28.
+//
+
+import SwiftUI
+
+@main
+struct SocialProofSectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
