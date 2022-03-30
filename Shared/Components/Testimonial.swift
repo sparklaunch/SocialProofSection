a5ca8e5bcf9f86714b7c083c3c0f26c00d780421
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-03-30 13:03:04 +0900

    Complete the UI.

--- Shared/Components/Testimonial.swift
+++ Shared/Components/Testimonial.swift
@@ -15,7 +15,7 @@ struct Testimonial: View {
     var body: some View {
         ZStack {
             Color("TitleColor")
-            VStack(alignment: .leading, spacing: 20) {
+            VStack(alignment: .leading, spacing: 25) {
                 HStack(spacing: 30) {
                     Image(profileImage)
                         .resizable()
@@ -23,23 +23,20 @@ struct Testimonial: View {
                         .clipShape(Circle())
                     VStack(alignment: .leading, spacing: 5) {
                         Text(name)
-                            .font(.title2)
                             .bold()
                             .customFont(fontSize: 20, relativeTo: .title2)
                             .foregroundColor(.white)
                         Text(occupation)
-                            .font(.title3)
                             .fontWeight(.ultraLight)
                             .customFont(fontSize: 18, relativeTo: .title3)
                             .foregroundColor(.white)
                     }
                 }
                 Text("\" " + description + " \"")
-                    .font(.body)
                     .customFont(fontSize: 16, relativeTo: .body)
                     .foregroundColor(.white)
                     .fixedSize(horizontal: false, vertical: true)
-                    .lineSpacing(5)
+                    .lineSpacing(8)
             }
             .padding(40)
         }
